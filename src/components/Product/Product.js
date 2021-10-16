f502fafb1b25de4aec98ff344d3bb5707c309235
motiurrahmanmisu <motiurrahmanmisu@gmail.com>
2021-10-16 14:02:11 +0600

    add rating

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
-
-
 import './Product.css'
+import Rating from 'react-rating';
+
 
 const Product = (props) => {
-    const { name, img, seller, price, stock } = props.product;
+    const { name, img, seller, price, stock, star } = props.product;
     const cartIcon = <FontAwesomeIcon icon={faShoppingCart} />
     return (
         <div className="product">
@@ -18,6 +18,13 @@ const Product = (props) => {
                 <p><small>BY {seller}</small></p>
                 <h3>${price}</h3>
                 <p>only {stock} left in stock - order soon</p>
+                <Rating
+                    initialRating={star}
+                    emptySymbol="far fa-star icon-color"
+                    fullSymbol="fas fa-star icon-color"
+                    readonly
+                ></Rating>
+                <br />
                 <button onClick={() => props.handleAddToCart(props.product)} className="btn-regular"> {cartIcon}Add to cart</button>
             </div>
         </div>
